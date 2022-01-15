ae54e24cfe126936e3ba6c29d6f17fb7982dfc35
HP <HP@LAPTOP-SMC8HIAI>
2022-01-15 09:54:28 +0530

    new commit 5

---
+++ automationqsp/src/assignments/ExplicitlyWait2.java
@@ -0,0 +1,35 @@
+package assignments;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class ExplicitlyWait2 {
+	
+	public static void main(String[] args) throws InterruptedException {
+		System.setProperty("webdriver.gecko.driver", "./drivers/geckodriver.exe");
+		WebDriver driver=new FirefoxDriver();
+		driver.manage().window().maximize();
+		driver.get("http://laptop-smc8hiai/login.do");//actitime login
+		
+		WebDriverWait wait = new WebDriverWait(driver, 30);
+		wait.until(ExpectedConditions.titleContains("Login"));
+		System.out.println(" THE LOGIN PAGE TITLE IS MATCHED , TEST CASE IS PASSED");
+		
+		driver.findElement(By.name("username")).sendKeys("admin");
+		driver.findElement(By.name("pwd")).sendKeys("manager");
+		driver.findElement(By.id("loginButton")).click();
+		
+		wait.until(ExpectedConditions.titleContains("Enter"));
+		System.out.println(" THE Home PAGE TITLE IS MATCHED , TEST CASE IS PASSED");
+		
+		
+		
+		
+		
+}
+
+}
