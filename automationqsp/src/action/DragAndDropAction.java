f3e689ef65b6531089bb4cdd879a7d2fdab37e42
HP <HP@LAPTOP-SMC8HIAI>
2022-01-20 09:49:25 +0530

    new8

---
+++ automationqsp/src/action/DragAndDropAction.java
@@ -0,0 +1,30 @@
+package action;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+public class DragAndDropAction {
+	
+	
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver", "./drivers/chromedriver.exe");
+		WebDriver driver=new ChromeDriver();
+		driver.get("file:///C:/Users/HP/Desktop/newseleniumbatch/dragdrop.html");
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		
+		WebElement source = driver.findElement(By.id("draggable-2"));
+		WebElement target = driver.findElement(By.id("droppable-2"));
+		
+		Actions act = new Actions(driver);
+		act.dragAndDrop(source, target).perform();
+	}
+	
+	
+
+}
---
+++ automationqsp/src/methodsOfWebElements/GetLocactionMethod.java
@@ -0,0 +1,30 @@
+package methodsOfWebElements;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Point;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class GetLocactionMethod{
+	
+	
+	public static void main(String[] args) throws InterruptedException {
+		System.setProperty("webdriver.chrome.driver", "./drivers/chromedriver.exe");	WebDriver driver=new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://www.google.com");
+		WebElement searchBox = driver.findElement(By.name("q"));
+	
+		Point loc = searchBox.getLocation();
+		int xaxis = loc.getX();
+		int yaxis = loc.getY();
+		
+		System.out.println("the location of webelement is "+xaxis+ " "+yaxis);
+		
+	
+
+	}
+	
+	
+
+}
