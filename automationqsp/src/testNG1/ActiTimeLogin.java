a74175c55bdc8f458f916011caa472c7936e6724
HP <HP@LAPTOP-SMC8HIAI>
2022-02-21 10:34:43 +0530

    nccc

---
+++ automationqsp/src/testNG1/ActiTimeLogin.java
@@ -0,0 +1,28 @@
+package testNG1;
+
+import java.util.concurrent.TimeUnit;
+
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+public class ActiTimeLogin {
+	WebDriver driver;
+	@Test
+	@Parameters({"username","password"})
+	public void actiTimeLogin(String username,String password) 
+	{
+		System.setProperty("webdriver.chrome.driver", "./drivers/chromedriver.exe");
+		driver=new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(40,TimeUnit.SECONDS);
+		driver.get("http://laptop-smc8hiai/login.do");
+		driver.findElement(By.name("username")).sendKeys(username);
+		driver.findElement(By.name("pwd")).sendKeys(password);
+		driver.findElement(By.id("loginButton")).click();
+		
+	}
+}
